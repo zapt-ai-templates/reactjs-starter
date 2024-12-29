df48f9031ad3d627b3d1a3f62569e3e1f154de10
David Fox <davidmfox123@gmail.com>
2024-12-29 22:50:37 +0000

    add default PWA support

--- src/index.jsx
+++ src/index.jsx
@@ -15,6 +15,19 @@ Sentry.init({
   },
 });
 
+// Add PWA support
+window.progressierAppRuntimeSettings = {
+  uid: import.meta.env.VITE_PUBLIC_APP_ID,
+  icon512: "https://supabase.zapt.ai/storage/v1/render/image/public/icons/c7bd5333-787f-461f-ae9b-22acbc0ed4b0/55145115-0624-472f-96b9-d5d88aae355f.png?width=512&height=512",
+  name: 'New App',
+  shortName: 'New App',
+};
+
+let progressierScript = document.createElement('script');
+progressierScript.setAttribute('src', 'https://progressier.app/z8yY3IKmfpDIw3mSncPh/script.js');
+progressierScript.setAttribute('defer', 'true');
+document.querySelector('head').appendChild(progressierScript);
+
 // Umami Analytics
 if (!window.location.hostname.includes('vercel.app')) {
   const script = document.createElement('script');
