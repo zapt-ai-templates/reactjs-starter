cc0006a2cb4ef65408ca011aa20f4b2f5add2b82
David Fox <davidmfox123@gmail.com>
2024-12-30 11:30:33 +0000

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import * as Sentry from '@sentry/browser';
 Sentry.init({
   dsn: import.meta.env.VITE_PUBLIC_SENTRY_DSN,
   environment: import.meta.env.VITE_PUBLIC_APP_ENV,
+  integrations: [Sentry.browserTracingIntegration()],
   initialScope: {
     tags: {
       type: 'frontend',
