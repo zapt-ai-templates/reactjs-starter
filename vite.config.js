0e3a347a4167262b876796f422a8575993fedfc0
David Fox <davidmfox123@gmail.com>
2025-01-14 22:51:13 +0000

    add sourcemap to Sentry

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite';
-import react from "@vitejs/plugin-react"
+import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "zapt-apps",
+      project: process.env.VITE_PUBLIC_APP_ID,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
+    sourcemap: true
   },
   resolve: {
     conditions: ['development', 'browser'],
+  },
+  optimizeDeps: {
+    exclude: ['drizzle-orm']
   }
 });
